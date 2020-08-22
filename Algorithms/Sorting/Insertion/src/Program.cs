84ee4f95a8ae0e9e64678ab0a3b9c53442e32b68
Jeffrey Mante <jeffreymante@hotmail.co.uk>
2020-08-23 00:36:37 +0100

    wip

--- Algorithms/Sorting/Insertion/src/Program.cs
+++ Algorithms/Sorting/Insertion/src/Program.cs
@@ -6,7 +6,51 @@ namespace Insertion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = { 10, 2, 3, 3, 23, 43, 1 };
+            Print(array);
+            InsertionSort(ref array);
+            Print(array);
+        }
+
+        static void InsertionSort(ref int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Insert(0, i, array[i], ref array);
+            }
+        }
+
+        static void Print(int[] array)
+        {
+            Console.WriteLine();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i]);
+                if (i < array.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+                else
+                {
+                    Console.Write(Environment.NewLine);
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        static void Insert(int rangeStart, int rangeEnd, int value, ref int[] array)
+        {
+            var index = rangeEnd;
+
+            for (int i = rangeEnd; i > rangeStart && array[i - 1] > value; i--)
+            {
+                array[i] = array[i - 1];
+                index--;
+            }
+
+            array[index] = value;
         }
     }
 }
